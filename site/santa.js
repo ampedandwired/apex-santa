a769300a5c2ddf2c73fe207c005ae7ed76af1f2a
Charles Blaxland <charles.blaxland@gmail.com>
2016-07-12 02:15:16 +1000

    Nicely formatted dates

--- site/santa.js
+++ site/santa.js
@@ -11,21 +11,24 @@ var santa = (function($) {
       id: "berowra-heights-2016",
       kml: "berowra-heights2-2016.kml",
       title: "Berowra Heights",
-      start_time: "2016-12-20 19:00:00",
+      start_time: "2016-12-16 19:00:00",
+      start_time_nice: "Friday 16th December, 7pm",
       description: "Starts at Coles and finishes at Warrina Oval. Come and join us afterwards at Warrina Oval for a BBQ."
     },
     {
       id: "berowra-2016",
       kml: "berowra2-2016.kml",
       title: "Berowra",
-      start_time: "2016-12-21 19:00:00",
+      start_time: "2016-12-17 18:00:00",
+      start_time_nice: "Saturday 17th December, 6pm",
       description: "Starts at Coles and finishes at Berowra Oval. There will be a BBQ, jumping castle and Santa photos at the oval afterwards."
     },
     {
       id: "blacktown-2016",
       kml: "berowra2-2016.kml",
       title: "Blacktown",
-      start_time: "2016-12-22 19:00:00",
+      start_time: "2016-12-18 14:00:00",
+      start_time_nice: "Sunday 18th December, 2pm",
       description: "Cruise the streets of Blacktown"
     }
   ];
