244f0eb09553f353e601d7c3177d6fcf3d7e4a41
Charles Blaxland <charles.blaxland@gmail.com>
2016-11-04 14:32:39 +1100

    Cleanup a few things

--- site/events.js
+++ site/events.js
@@ -2,22 +2,22 @@ var santa_events = [
   {
     id: "1mIS81SeIlT-NVMl1LSlZCETdTvU",
     title: "Berowra Heights",
-    start_time: "2016-12-16 19:00:00+11",
-    start_time_nice: "Friday 16th December, 7pm",
-    description: "Starts at Coles and finishes at Warrina Oval. Come and join us afterwards at Warrina Oval for a BBQ."
+    start_time: "2016-12-09 18:00:00+11",
+    start_time_nice: "Friday 9th December, 6pm",
+    description: "Santa will cruise the streets of Berowra."
   },
   {
     id: "1XHsOpkQoQW5SqNT4gTS3Vp07NNQ",
-    title: "Berowra",
-    start_time: "2016-12-17 18:00:00+11",
-    start_time_nice: "Saturday 17th December, 6pm",
-    description: "Starts at Berowra District Hall and finishes at Berowra Oval. There will be a BBQ, jumping castle and Santa photos at the oval afterwards."
+    title: "Warrina Oval",
+    start_time: "2016-12-15 18:00:00+11",
+    start_time_nice: "Thursday 15th December, 6pm",
+    description: "Starts at Coles and finishes at Warrina Oval. Come and join us afterwards at Warrina Oval for a BBQ."
   },
   {
     id: "1iIDFz8Tphh-xXaHMVMLGfv4QTA0",
-    title: "Blacktown",
-    start_time: "2016-12-18 14:00:00+11",
-    start_time_nice: "Sunday 18th December, 2pm",
-    description: "Cruise the streets of Blacktown, brought to you by <a href=\"http://www.apexblacktown.com/\">Blacktown Apex</a>"
+    title: "Berowra Oval",
+    start_time: "2016-12-22 18:00:00+11",
+    start_time_nice: "Thursday 22nd December, 6pm",
+    description: "Starts at Berowra District Hall and finishes at Berowra Oval. There will be a BBQ, jumping castle and Santa photos at the oval afterwards."
   }
 ];
