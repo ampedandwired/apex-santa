34cc63f220b8732e3a15806b2289fb1a1bce4627
Charles Blaxland <charles.blaxland@gmail.com>
2017-11-20 22:05:31 +1100

    Update events for 2017

--- site/events.js
+++ site/events.js
@@ -2,29 +2,29 @@ var santa_events = [
   {
     id: "1mIS81SeIlT-NVMl1LSlZCETdTvU",
     title: "Crossroads Reserve",
-    start_time: "2016-12-09T17:00:00+11:00",
-    start_time_nice: "Friday 9th December, 5pm",
+    start_time: "2017-12-08T17:00:00+11:00",
+    start_time_nice: "Friday 8th December, 5pm",
     description: "Barnetts and other areas west of Berowra Waters Road. Join Santa at Crossroads Reserve from 6pm for photos. Christmas trees will also be on sale at the reserve."
   },
   {
     id: "1XHsOpkQoQW5SqNT4gTS3Vp07NNQ",
-    title: "Warrina Oval (CANCELLED)",
-    start_time: "2016-12-15T18:00:00+11:00",
-    start_time_nice: "Thursday 15th December, 5pm",
-    description: "THIS EVENT HAS BEEN CANCELLED DUE TO INCLEMENT WEATHER. WE WILL ATTEMPT TO RESCHEDULE NEXT WEEK."
+    title: "Warrina Oval",
+    start_time: "2017-12-14T17:00:00+11:00",
+    start_time_nice: "Thursday 14th December, 5pm",
+    description: "Turner, Woodcourt, Wideview, Lonsdale and Alan Road. Join Santa at Warrina Oval from 6pm for photos, BBQ and jumping castle."
   },
   {
     id: "1iIDFz8Tphh-xXaHMVMLGfv4QTA0",
     title: "Berowra Oval",
-    start_time: "2016-12-22T18:00:00+11:00",
-    start_time_nice: "Thursday 22nd December, 5pm",
+    start_time: "2017-12-21T17:00:00+11:00",
+    start_time_nice: "Thursday 21st December, 5pm",
     description: "Gully Rd and Berowra. Join Santa at Berowra Oval from 5pm for photos, BBQ and jumping castle."
   },
   {
     id: "19DPVg5mIFcWpu__-iB9rnkraKLI",
     title: "Cowan",
-    start_time: "2016-12-24T14:50:00+11:00",
-    start_time_nice: "Saturday 24th December, 2:50pm",
+    start_time: "2017-12-23T15:00:00+11:00",
+    start_time_nice: "Saturday 23rd December, 3pm",
     description: "Santa joins the Cowan RFS for a jaunt round the streets of Cowan, finishing up at the general store"
   }
 ];
