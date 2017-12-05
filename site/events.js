7de8b8f5fadfd8e48a5dedaaa9b98b2774f7c5f4
Charles Blaxland <charles.blaxland@gmail.com>
2017-12-06 08:59:28 +1100

    Update events

--- site/events.js
+++ site/events.js
@@ -1,10 +1,10 @@
 var santa_events = [
   {
-    id: "1mIS81SeIlT-NVMl1LSlZCETdTvU",
+    id: "1Sz4LoLzQMNvKCq0F3HWrOB0vFNCVrUHr",
     title: "Crossroads Reserve",
     start_time: "2017-12-08T17:00:00+11:00",
-    start_time_nice: "Friday 8th December, 5pm",
-    description: "Barnetts and other areas west of Berowra Waters Road. Join Santa at Crossroads Reserve from 6pm for photos. Christmas trees will also be on sale at the reserve."
+    start_time_nice: "Friday 8th December, 6pm",
+    description: "Barnetts and other areas west of Berowra Waters Road finishing at Crossroads Reserve, with a special stop at Berowra Public School Christmas carols! Christmas trees will also be on sale at Crossroads Reserve."
   },
   {
     id: "1XHsOpkQoQW5SqNT4gTS3Vp07NNQ",
@@ -18,7 +18,7 @@ var santa_events = [
     title: "Berowra Oval",
     start_time: "2017-12-21T17:00:00+11:00",
     start_time_nice: "Thursday 21st December, 5pm",
-    description: "Gully Rd and Berowra. Join Santa at Berowra Oval from 5pm for photos, BBQ and jumping castle."
+    description: "Gully Rd and Berowra. Join Santa at Berowra Oval from 6pm for photos, BBQ and jumping castle."
   },
   {
     id: "19DPVg5mIFcWpu__-iB9rnkraKLI",
