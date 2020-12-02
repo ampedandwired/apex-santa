b83d1ed14fa5e6e16b942f42a8ac833989da7481
Charles Blaxland <charles.blaxland@gmail.com>
2020-12-02 11:10:37 +1100

    2020 updates

--- site/events.js
+++ site/events.js
@@ -1,23 +1,50 @@
 var santa_events = [
   {
-    id: "1Sz4LoLzQMNvKCq0F3HWrOB0vFNCVrUHr",
-    title: "Crossroads Reserve",
-    start_time: "2019-12-06T17:30:00+11:00",
-    start_time_nice: "Friday 6th December, 5:30pm",
-    description: "Barnetts and surrounding areas finishing at Crossroads Reserve. Christmas trees will also be on sale at Crossroads Reserve, and again on Saturday morning."
+    id: "1cty0noBpUQ4NyQHnRKywFdJ6EJ3BD42Y",
+    title: "Barnetts",
+    start_time: "2020-12-04T17:30:00+11:00",
+    start_date_nice: "Friday 4th December",
+    start_time_nice: "5:30pm",
+    description: "Berkley, Clinton, Barnetts, Wyanna, Easton, Jolah, Holliday, Arcadia, finishing at Crossroads Reserve. Christmas trees will be on sale at Crossroads Reserve, and again on Saturday morning."
   },
   {
-    id: "1XHsOpkQoQW5SqNT4gTS3Vp07NNQ",
-    title: "Warrina Area",
-    start_time: "2019-12-13T17:00:00+11:00",
-    start_time_nice: "Friday 13th December, 5pm",
-    description: "Turner, Woodcourt, Wideview, Lonsdale and Alan Road. NOTE: There will be NO BBQ after this run."
+    id: "1AYqEFdwjilsrRo_23cHTZoTAg3L6cjsl",
+    title: "Warrina",
+    start_time: "2020-12-07T17:00:00+11:00",
+    start_date_nice: "Monday 7th December",
+    start_time_nice: "5pm",
+    description: "Warrina, Koloona, Balaclava, Rosslyn, Koorong"
   },
   {
-    id: "1iIDFz8Tphh-xXaHMVMLGfv4QTA0",
-    title: "Berowra Oval + BBQ",
-    start_time: "2019-12-18T17:00:00+11:00",
-    start_time_nice: "Wednesday 18th December, 5pm",
-    description: "Gully Rd and Berowra. Join Santa at Berowra Oval from 6pm for photos, BBQ and jumping castle."
+    id: "1ntf_c5oCezAC7odmshNx0cXBNiG6uRpb",
+    title: "Yallambee",
+    start_time: "2020-12-11T17:00:00+11:00",
+    start_date_nice: "Friday 11th December",
+    start_time_nice: "5pm",
+    description: "Crowley, Cumbora, Waratah, Anembo, Yallambee, Kywong, Redwood, Bambil, Greenview"
+  },
+  {
+    id: "1ANAszTd121BrgJlT5rfN8QsL6MF6YLPz",
+    title: "Wideview",
+    start_time: "2020-12-16T17:00:00+11:00",
+    start_date_nice: "Wednesday 16th December",
+    start_time_nice: "5pm",
+    description: "Turner, Cambewarra, Elizabeth, Lingellen, Alan, Warruga, Wideview, Evelyn, Lonsdale, Woodcourt"
+  },
+  {
+    id: "1wpCmz6-GP4q6Aqy6qeybH4fqLEuTfBNX",
+    title: "Gully Rd",
+    start_time: "2020-12-18T17:00:00+11:00",
+    start_date_nice: "Friday 18th December",
+    start_time_nice: "5pm",
+    description: "Berowra Waters Rd, Gully, Willunga, Richards, Mary Wall, Glencoe"
+  },
+  {
+    id: "1T6vwkyT5aW6d8kqj1TSTLFpyTAASlLix",
+    title: "Mystery Run",
+    start_time: "2020-12-22T17:00:00+11:00",
+    start_date_nice: "Tuesday 22nd December",
+    start_time_nice: "5pm",
+    description: "Santa will be out and about around Berowra somewhere, keep your eye out for him!"
   }
 ];
