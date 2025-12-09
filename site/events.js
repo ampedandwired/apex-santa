e210d595ceb13980bf042e2fee0be3ce12b0a9bb
Charles Blaxland <charles.blaxland@gmail.com>
2025-12-09 11:05:03 +1100

    Update events

--- site/events.js
+++ site/events.js
@@ -1,26 +1,26 @@
 var santa_events = [
-  {
-    id: "1XHsOpkQoQW5SqNT4gTS3Vp07NNQ",
-    title: "Berowra Heights North",
-    start_time: "2023-12-08T17:30:00+11:00",
-    start_date_nice: "Friday 8th December",
-    start_time_nice: "Run starts at 5:00pm",
-    description: "Turner, Wideview, Lonsdale, Woodcourt, Alan, Hillcrest, finishing at Warrina St Oval where you can meet Santa. Christmas trees will be on sale at the oval, and again on Saturday morning."
-  },
-  {
-    id: "1YXjL3Pq3gA-LM9hFb_tDLKYjyzgKefI",
-    title: "Berowra Heights South",
-    start_time: "2023-12-14T17:00:00+11:00",
-    start_date_nice: "Thursday 14th December",
-    start_time_nice: "Run starts at 5:00pm",
-    description: "Barnetts, Berkley, Wyanna, Jolah, Holliday, Balaclava, Koloona"
-  },
-  {
-    id: "1iIDFz8Tphh-xXaHMVMLGfv4QTA0",
-    title: "Berowra Oval",
-    start_time: "2023-12-22T17:00:00+11:00",
-    start_date_nice: "Friday 22nd December",
-    start_time_nice: "Santa run and BBQ starts 5pm, Santa on oval about 6pm",
-    description: "Crowley, Yallambee, Kywong, Gully. Free BBQ and festivities on Berowra Oval with Santa after!"
-  }
-];
+    {
+      id: "1kDzTK3BBAGGTIJRlCgAYISsOJCdg9V0",
+      title: "Berowra Heights North",
+      start_time: "2025-12-05T17:00:00+11:00",
+      start_date_nice: "Friday 5th December",
+      start_time_nice: "Run starts at 5:00pm, Markets at around 6:00pm",
+      description: "Turner, Wideview, Lonsdale, Woodcourt, Alan, Hillcrest, finishing with a visit to the Chrismas Night Markets at the Berowra Community Centre where you will be able to meet and take photos with Santa. Christmas trees will also be on sale at the markets, and again on Saturday morning."
+    },
+    {
+      id: "1YXjL3Pq3gA-LM9hFb_tDLKYjyzgKefI",
+      title: "Berowra Heights South",
+      start_time: "2025-12-11T17:00:00+11:00",
+      start_date_nice: "Thursday 11th December",
+      start_time_nice: "Run starts at 5:00pm",
+      description: "Barnetts, Berkley, Wyanna, Jolah, Holliday, Balaclava, Koloona"
+    },
+    {
+      id: "1iIDFz8Tphh-xXaHMVMLGfv4QTA0",
+      title: "Berowra Oval (w/ BBQ)",
+      start_time: "2025-12-18T17:00:00+11:00",
+      start_date_nice: "Thursday 18th December",
+      start_time_nice: "Santa run and BBQ starts 5pm, Santa on oval about 6pm",
+      description: "Crowley, Yallambee, Kywong, Gully. Free BBQ and festivities on Berowra Oval with Santa after!"
+    }
+  ];
